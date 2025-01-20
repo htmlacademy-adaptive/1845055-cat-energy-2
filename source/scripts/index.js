4538765c7ca84cf59477a4486b05c3731445a2c6
JuliaJYS <julysa83@gmail.com>
2025-01-20 11:31:03 +0300

    Добавила состояния и интерактивную карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav__list');
+const navToogle = document.querySelector('.main-nav__toggle');
+
+
+// navMain.classList.remove('main-nav__list--nojs');
+
+navToogle.addEventListener('click', () => {
+  if (navToogle.classList.contains('main-nav__toggle--close')) {
+    console.log('Close');
+    navMain.classList.remove('main-nav__list--opened');
+    navMain.classList.add('main-nav__list--nojs');
+    navToogle.classList.remove('main-nav__toggle--close');
+  } else {
+    console.log('Open');
+    navMain.classList.add('main-nav__list--opened');
+    navToogle.classList.add('main-nav__toggle--close');
+    navMain.classList.remove('main-nav__list--nojs');
+  }
+});
